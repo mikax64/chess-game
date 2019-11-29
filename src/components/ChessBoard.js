a47719e328883e895f81feeca69d3760e8e5d0d0
unknown <michael.belhatte@gmail.com>
2019-11-29 13:41:56 -0500

    initial commit

---
+++ src/components/ChessBoard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class ChessBoard extends Component {
+  componentDidMount() {
+    const axeX = ["a", "b", "c", "d", "e", "f", "g"];
+    const axeY = [1, 2, 3, 4, 5, 6, 7, 8];
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ChessBoard;
